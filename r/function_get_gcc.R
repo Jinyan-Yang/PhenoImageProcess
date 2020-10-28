4516f8471597c412a00220f5c1d57db316f3d0db
JinyanYang <jyyoung@live.com>
2020-10-28 12:30:52 +1100

    updated DN (Nyngan) GCC

--- r/function_get_gcc.R
+++ r/function_get_gcc.R
@@ -1,5 +1,8 @@
 source('r/functions.R')
 # modifed from Remko's function
+
+# fn <- 'pic/ng/9/WSCT1262.jpg'
+
 processImage.new <- function(fn, ROI=NULL){
   
   phot <- read_and_crop(fn,NULL)
@@ -17,6 +20,7 @@ processImage.new <- function(fn, ROI=NULL){
     phot <- phot[phot$x >= xmin & phot$x <= xmax &
                    phot$y >= ymin & phot$y <= ymax,]
   }
+  library(raster)
   
   RDN <- mean(phot$R)
   GDN <- mean(phot$G)
@@ -36,47 +40,51 @@ processImage.new <- function(fn, ROI=NULL){
 }
 
 get_gcc_func <- function(path.vec, ROI=NULL){
+  # path.vec='pic/ng/5/'
+  
   # read picture names
   nm.photo <- list.files(path.vec)
 
-  # remove files that are too dark
-  for(i in seq_along(nm.photo)){
-    fn <- file.path(path.vec,nm.photo[i])
-    
-    tmp <- try(readJPEG(fn))
-    
-    # remove dark photos and those cannot be read in
-    if(is.array(tmp)){
-      if(mean(tmp)<0.29){
-        unlink(fn)
-        print(fn)
-      }
-    }else{
-      unlink(fn)
-      print(fn)
-    }
-    
-  
-  }
+  # # remove files that are too dark
+  # for(i in seq_along(nm.photo)){
+  #   fn <- file.path(path.vec,nm.photo[i])
+  #   
+  #   tmp <- try(readJPEG(fn))
+  #   
+  #   # # remove dark photos and those cannot be read in
+  #   # if(is.array(tmp)){
+  #   #   # if(mean(tmp)<0.29){
+  #   #   #   unlink(fn)
+  #   #   #   print(fn)
+  #   #   # }
+  #   # }else{
+  #   #   unlink(fn)
+  #   #   print(fn)
+  #   # }
+  #   
+  # 
+  # }
 
   # reread picture names
   nm.photo <- list.files(path.vec,pattern = '.JPG')
   
-  # fn = 'download/irrigated/WSCT7365.JPG'
-  # library(exif)
-  # read_exif(fn)$origin_timestamp
+  # fn = c('download/irrigated/WSCT7365.JPG','download/irrigated/WSCT7366.JPG')
   
   # get the date
-  date.vec <- file.info(file.path(path.vec,nm.photo))$ctime
+  # date.vec <- file.info(file.path(path.vec,nm.photo))$ctime
   # date.vec <- parse_number(nm.photo)
+  fn = file.path(path.vec,nm.photo)
+  # fn=fn[1:10]
+  date.vec <- read_exif(fn)$CreateDate
 
   # set path for the processImages function
-  myimgs <- setImages(path.vec)
+  # myimgs <- setImages(path.vec)
+  # myimgs[[1]] <- myimgs[[1]][!duplicated(myimgs[[1]])]
   
   # read and calculate gcc
   temp.ls <- list()
-  for(i in seq_along(myimgs[[1]])){
-    temp.ls[[i]] <- processImage.new(myimgs[[1]][i], ROI=ROI)
+  for(i in seq_along(fn)){
+    temp.ls[[i]] <- processImage.new(fn[i], ROI=ROI)
   }
   
   # put gcc into a data frame
@@ -84,9 +92,23 @@ get_gcc_func <- function(path.vec, ROI=NULL){
   
   # gcc.day.df$DateTime <- as.Date(as.character(date.vec),'%Y%m%d')
   gcc.day.df$DateTime <- date.vec
-  gcc.day.df$Date <-  as.Date(date.vec)
-  
+  gcc.day.df$Date <-  as.Date(date.vec,'%Y:%m:%d')
+
   return(gcc.day.df)
   
+}
+
+
+get.smooth.gcc.func = function(Date.vec,gcc.vec){
+  library(mgcv)
+  library(lubridate)
+  gam.frdm = round(length(Date.vec)/3)
+  
+  gam.in.df = data.frame(x = as.numeric(Date.vec),
+                         y = gcc.vec)
+  fit.gam <- gam(y~s(x,k = gam.frdm),data = gam.in.df)
+  
+  out.df = predict(fit.gam,gam.in.df)
+  return(out.df)
 }
 
--- r/plot_aoi.R
+++ r/plot_aoi.R
@@ -6,8 +6,8 @@ photo.ls <- load.image(fn)
 # define coords
 # here I'm using propotional values
 # you can replace these with absoulte values
-x.coords <- c(0.0,0.5,0.5,0.0)*nrow(t1)
-y.coords <- c(0.1,0.1,0.9,0.9)*ncol(t1)
+x.coords <- c(0.0,0.5,0.5,0.0) * nrow(t1)
+y.coords <- c(0.1,0.1,0.9,0.9) * ncol(t1)
 
 # make plot
 plot(photo.ls,ann=F,axes=F)
